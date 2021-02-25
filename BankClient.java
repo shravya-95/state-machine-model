b8cfb3e09cc17dc6a7ec7243adf9fa22114b9767
Divya Shrinivasa Nairy <divyashrinivasanairy@divya.local>
2021-02-24 21:59:45 -0600

    logging and balance

--- BankClient.java
+++ BankClient.java
@@ -2,6 +2,7 @@ import java.rmi.RMISecurityManager;
 import java.rmi.Naming;
 import java.util.Date;
 
+//java  -Djava.security.policy=mySecurityPolicyfile BankClient localhost 12345
 public class BankClient {
     public static void main (String args[]) throws Exception {
         if ( args.length != 2 ) {
@@ -10,6 +11,11 @@ public class BankClient {
         System.setSecurityManager (new SecurityManager ());
         BankServer bankServer = (BankServer) Naming.lookup ("//" + args[0] + ":"+ args[1]+"/BankServer");
         //Same as before here
-        bankServer.createAccount();
+        int uid1 = bankServer.createAccount();
+//        int uid2 = bankServer.createAccount();
+
+        System.out.println(bankServer.deposit(uid1,100));
+        System.out.println(bankServer.getBalance(uid1));
+
     }
 }
