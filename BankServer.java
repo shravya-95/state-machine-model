ca6c9b7b22bd64d6fbed0433064a8754736fbc3b
Divya Nairy <1995divya@gmail.com>
2021-02-24 21:21:20 -0600

    Add files via upload
    bank server

---
+++ BankServer.java
@@ -0,0 +1,10 @@
+import java.rmi.Remote;
+import java.rmi.RemoteException;
+
+public interface BankServer extends Remote {
+  public int createAccount() throws RemoteException;
+  public boolean deposit(int uid, int amount) throws RemoteException;
+  public int getBalance(int uid) throws RemoteException;
+  public boolean transfer(int sourceUid, int targetUid, int amount) throws RemoteException;
+}
+
